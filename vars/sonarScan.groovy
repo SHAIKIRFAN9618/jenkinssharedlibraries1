9e129169b6543df2e87e6771af44f5966b34c780
shaikirfan9618 <irfan.shaik9618@gmail.com>
2025-05-20 20:53:04 +0530

    KK

--- vars/sonarScan.groovy
+++ vars/sonarScan.groovy
@@ -2,7 +2,7 @@ def call() {
     stage('SonarQube Analysis') {
         withSonarQubeEnv('SonarQubeServer') {
             // Explicitly load Maven tool to ensure mvn command is available
-            def mvnHome = tool 'Maven 3.9.9'  // Make sure this matches what you configured in Jenkins
+            def mvnHome = tool 'maven'  // Make sure this matches what you configured in Jenkins
             withEnv(["PATH+MAVEN=${mvnHome}/bin"]) {
                 sh 'mvn sonar:sonar'
             }
